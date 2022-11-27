4bbcccf0e90768146e20d84fe6b093a39fde7a68
Dmitriy Malakhov <d.malakhov.v@gmail.com>
2022-11-27 18:15:04 +0300

    Настройка маршрутов

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'calculator/input'
-  get 'calculator/result'
+  root 'calculator#input'
+  post '/result', to: 'calculator#result'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
