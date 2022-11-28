6db2f3501b1c46ca8708d8172ca3819f5a40725e
Dmitriy Malakhov <d.malakhov.v@gmail.com>
2022-11-28 19:49:24 +0300

    Обработка данных из формы без валидаций

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -3,5 +3,8 @@ class CalculatorController < ApplicationController
   end
 
   def result
+    x_value = params[:x].to_f # достаем значение x и приводим его к числу
+    y_value = params[:y].to_f # достаем значение y и приводим его к числу
+    @result = x_value.send(params[:operation], y_value) # считаем результат
   end
 end
